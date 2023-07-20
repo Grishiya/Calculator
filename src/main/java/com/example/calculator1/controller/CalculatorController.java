a1ba3220483dd236d8cf1defcba96e80d7e3662d
Grisha <guruwolf8@gmail.com>
2023-07-20 18:38:14 +0300

    Просто сохраняю

--- src/main/java/com/example/calculator1/controller/CalculatorController.java
+++ src/main/java/com/example/calculator1/controller/CalculatorController.java
@@ -1,23 +1,47 @@
 package com.example.calculator1.controller;
 
 import com.example.calculator1.service.CalculatorService;
+import com.example.calculator1.service.CalculatorServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
-    private CalculatorService calculatorService;
+    private final CalculatorService calculatorService;
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
 
-@GetMapping
-public String dobro(){return "Хочешь увидеть магию? Тогда добавь в адресную строку /calculator";
-}
-    @GetMapping("/calculator")
+
+    @GetMapping
     public String welcome() {
-        return "Добро пожаловать в калькулятор";
+
+        return calculatorService.welcome();
     }
 
+    @GetMapping("/plus")
+    public String plus(@RequestParam (required = false) Integer num1, @RequestParam int num2) {
+        if(num1==null){
+            num1=0;
+        }
+        return calculatorService.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return calculatorService.minus(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return calculatorService.multiply(num1, num2);
+    }
+
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        return calculatorService.divide(num1, num2);
+    }
 }
--- src/main/java/com/example/calculator1/service/CalculatorServiceImpl.java
+++ src/main/java/com/example/calculator1/service/CalculatorServiceImpl.java
@@ -6,25 +6,29 @@ import org.springframework.stereotype.Service;
 public class CalculatorServiceImpl implements CalculatorService {
     @Override
     public String welcome(){
-        return "Хочешь увидеть магию? Тогда добавь в адресную строку /calculator";
+        return "<big>Добро пожаловать в калькулятор !</big>";
     }
     @Override
     public String plus(int num1, int num2) {
-        return null;
+        int num3=num1+num2;
+        return num1+ "+"+num2+"="+num3;
     }
 
     @Override
     public String minus(int num1, int num2) {
-        return null;
+        int num3=num1-num2;
+        return num1+ "-"+num2+"="+num3;
     }
 
     @Override
     public String multiply(int num1, int num2) {
-        return null;
+        int num3=num1*num2;
+        return num1+ "*"+num2+"="+num3;
     }
 
     @Override
     public String divide(int num1, int num2) {
-        return null;
+        int num3=num1/num2;
+        return num1+ "/"+num2+"="+num3;
     }
 }
